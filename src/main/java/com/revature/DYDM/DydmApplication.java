fbc72b7b2cb45bb636ff1cf403f083a06571fded
Brett Davis <brett.davis5511@gmail.com>
2022-01-10 15:37:13 -0600

    Base Spring Application

---
+++ src/main/java/com/revature/DYDM/DydmApplication.java
@@ -0,0 +1,13 @@
+package com.revature.DYDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DydmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DydmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/DYDM/DydmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.DYDM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DydmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
